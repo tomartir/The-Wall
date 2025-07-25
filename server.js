62adaeb9fbe8775b217cd4442e21322ce49bc6ce
tomartir <tomartir@gmail.com>
2025-07-25 17:17:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve la cartella frontend (modifica se il percorso non è ../frontend)
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname)));
 
 // Middleware per JSON e CORS
 app.use(cors());
